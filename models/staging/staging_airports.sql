f72a7a32d315a0019561dbff2dbea4f962796691
Kai Naegle <kainaegle@Kais-MacBook-Pro.local>
2026-04-21 17:01:30 +0200

    whatever

--- models/staging/staging_airports.sql
+++
@@ -1,7 +0,0 @@
-WITH airports_regions_join AS (
-    SELECT * 
-    FROM {{source('project_data', 'airports')}}
-    LEFT JOIN {{source('project_data', 'regions')}}
-    USING (country)
-)
-SELECT * FROM airports_regions_join
